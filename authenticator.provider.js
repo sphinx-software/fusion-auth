b2220206c1ca7a0cc1cc13fda2ff6456f25c5758
NgocTo <tongoc92@gmail.com>
2018-01-12 11:37:34 +0700

    move step register session.started event to boot

--- authenticator.provider.js
+++ authenticator.provider.js
@@ -7,13 +7,7 @@ module.exports.register = async (container) => {
     const config = await container.make('config');
 
     container.singleton('auth', async () => {
-        let auth = new Auth(config.auth.session.credentialKey);
-        let events = await container.make('events');
-
-        // authenticator set session as a dependence when session already started
-        events.on('session.started', session => auth.setSession(session));
-
-        return auth;
+        return new Auth(config.auth.session.credentialKey);
     });
 
     container.singleton('auth.credential-provider', async () => {
@@ -32,10 +26,13 @@ module.exports.register = async (container) => {
 
 module.exports.boot = async (container) => {
     const serializer = await container.make('serializer');
+    const auth       = await container.make('auth');
+    const events     = await container.make('events');
 
-    /**
-     * register Credential type
-     */
+    // authenticator set session as a dependence when session already started
+    events.on('session.started', session => auth.setSession(session));
+
+    // register Credential type
     serializer.forType(Credential, (credential) => {
         return credential.getIdentity();
     }, (raw) => {
